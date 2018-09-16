a0d296430b17c3af08379157d635312dd2e20409
Oganesyan82 <ilya.oganesyan.82@mail.ru>
2018-09-16 12:12:34 +0300

    Lesson 1 - hometask 2 .1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,10 +11,21 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4,6);
     System.out.println("Площадь прямоугольника с сторонами " + r.a + " и " + r.b + " равна " + s.area());
+
+    Point p1 = new Point();
+    Point p2 = new Point();
+    p1.x = 1; p1.y = 1;
+    p2.x = 5; p2.y = 4;
+    System.out.println("Расстояние между двумя точками p1 (" + p1.x + " ; " + p1.y + ") и p2 (" + p2.x + " ; " + p2.y + ") равно " + distance(p1,p2));
+
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt( (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y) );
   }
 
 }

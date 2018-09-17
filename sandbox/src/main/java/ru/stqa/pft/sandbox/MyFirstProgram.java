95efefef518cdc80881655f2eed2a7febb2fd8ce
Oganesyan82 <ilya.oganesyan.82@mail.ru>
2018-09-17 19:45:29 +0300

    Lesson 1 - Hometask 2.2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -10,22 +10,16 @@ public class MyFirstProgram {
     System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
     Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника с сторонами " + r.a + " и " + r.b + " равна " + s.area());
+    System.out.println("Площадь прямоугольника с сторонами " + r.a + " и " + r.b + " равна " + r.area());
 
     Point p1 = new Point(1 , 1);
-    Point p2 = new Point(5 , 4);
-    //Point dist = new Point(p1, p2);
+    Point p2 = new Point(4 , 5);
 
-    System.out.println("Расстояние между двумя точками p1 (" + p1.x + " ; " + p1.y + ") и p2 (" + p2.x + " ; " + p2.y + ") равно " + distance(p1, p2));
-
+    System.out.println("Расстояние между двумя точками p1 (" + p1.x + " ; " + p1.y + ") и p2 (" + p2.x + " ; " + p2.y + ") равно " + p1.distance(p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
   }
 
 }
